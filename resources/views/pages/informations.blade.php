e28aba8229f196697654c6ea422f40b9b54691dc
MOUSTAID-Youssef <youssef.mstd@gmail.com>
2025-02-21 12:17:42 +0100

    front end done

--- resources/views/pages/informations.blade.php
+++ resources/views/pages/informations.blade.php
@@ -1,79 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="margin-left: 18px;text-align:center" >
+<div class="container mt-5" style="text-align: center;">
 
-    <div class="d-flex align-items-center justify-content-between" style="margin-bottom: 15px;">
-        <h1 class="me-3">Liste des membres</h1>
-        <a href="" class="btn btn-success">
-            <i class="fas fa-user-plus"></i> Ajouter
+    <div class="d-flex align-items-center justify-content-between mb-3">
+        <h1 class="me-3">Détails de l'entreprise</h1>
+        <a href="" class="btn btn-warning">
+            <i class="fas fa-edit"></i> Modifier
         </a>
     </div>
-    
-    
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Prénom</th>
-                <th>Login</th>
-                <th>Privilège</th>
-                <th>Ville</th>
-                <th>Etat</th>
-                <th>Consulter</th>
-                <th>Modifier</th>
-                <th>Supprimer</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>abdelali</td>
-                <td>ghardgui</td>
-                <td>rdfradeema</td>
-                <td>admin</td>
-                <td>Marrakech</td>
-                <td>actif</td>
-                <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
-                <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
-                <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
-            </tr>
-            <tr>
-                <td>ACHBAROU</td>
-                <td>MOUHCINE</td>
-                <td>ACHBAROU</td>
-                <td>user</td>
-                <td>Taza</td>
-                <td>actif</td>
-                <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
-                <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
-                <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
-            </tr>
-            <tr>
-                <td>BENANI</td>
-                <td>ADIL</td>
-                <td>RADEEF2016</td>
-                <td>user</td>
-                <td>Fès</td>
-                <td>actif</td>
-                <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
-                <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
-                <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
-            </tr>
-            <tr>
-                <td>bensediq</td>
-                <td>farid</td>
-                <td>bensediq</td>
-                <td>admin</td>
-                <td>Oujda</td>
-                <td>actif</td>
-                <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
-                <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
-                <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
-            </tr>
-        </tbody>
-    </table>
-    <div>
+    <div class="card shadow-lg" style="width: 100%; border-radius: 10px;">
+        <div class="card-body">
+            <p class="card-text">
+                <strong>Raison sociale:</strong> WATEC
+            </p>
+            <hr>
+            <p class="card-text">
+                <strong>Adresse:</strong> Lot 282 Zone Industriel Sud Ouest Mohammedia
+            </p>
+            <hr>
+            <p class="card-text">
+                <i class="fas fa-phone-alt me-2"></i><strong>Tél:</strong> +212 (0) 5 23 30 11 01
+            </p>
+            <p class="card-text">
+                <i class="fas fa-envelope me-2"></i><strong>Email:</strong> info@watec.ma
+            </p>
+        </div>
+    </div>
+
+</div>
 @endsection
-
-
--- resources/views/pages/utilisateur.blade.php
+++ resources/views/pages/utilisateur.blade.php
@@ -1,18 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="margin-left: 18px;text-align:center" >
-
+<div style="margin-left: 18px; text-align:center">
     <div class="d-flex align-items-center justify-content-between" style="margin-bottom: 15px;">
         <h1 class="me-3">Liste des utilisateurs</h1>
         <a href="" class="btn btn-success">
-            <i class="fas fa-user-plus"></i> Ajouter
+            <i class="fas fa-user-plus"></i> Ajouter utilisateur
         </a>
     </div>
-    
-    
-
-    <table class="table table-striped">
+    <table class="table table-striped table-fixed">
         <thead>
             <tr>
                 <th>Nom</th>
@@ -28,22 +24,11 @@
         </thead>
         <tbody>
             <tr>
-                <td>abdelali</td>
-                <td>ghardgui</td>
-                <td>rdfradeema</td>
-                <td>admin</td>
-                <td>Marrakech</td>
-                <td>actif</td>
-                <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
-                <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
-                <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
-            </tr>
-            <tr>
-                <td>ACHBAROU</td>
-                <td>MOUHCINE</td>
-                <td>ACHBAROU</td>
+                <td>BENANI</td>
+                <td>ADIL</td>
+                <td>RADEEF2016</td>
                 <td>user</td>
-                <td>Taza</td>
+                <td>Fès</td>
                 <td>actif</td>
                 <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
                 <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
@@ -61,11 +46,22 @@
                 <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
             </tr>
             <tr>
-                <td>bensediq</td>
-                <td>farid</td>
-                <td>bensediq</td>
-                <td>admin</td>
-                <td>Oujda</td>
+                <td>BENANI</td>
+                <td>ADIL</td>
+                <td>RADEEF2016</td>
+                <td>user</td>
+                <td>Fès</td>
+                <td>actif</td>
+                <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
+                <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
+                <td><button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
+            </tr>
+            <tr>
+                <td>BENANI</td>
+                <td>ADIL</td>
+                <td>RADEEF2016</td>
+                <td>user</td>
+                <td>Fès</td>
                 <td>actif</td>
                 <td><button class="btn btn-info"><i class="fas fa-eye"></i></button></td>
                 <td><button class="btn btn-warning"><i class="fas fa-edit"></i></button></td>
@@ -73,7 +69,8 @@
             </tr>
         </tbody>
     </table>
-    <div>
+</div>
 @endsection
+
 
 
